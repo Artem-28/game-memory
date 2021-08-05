2bd19f559433eaaf10d1eba6abb167c796458124
Artem-28 <artem.mikheev.git@gmail.com>
2021-08-05 09:08:07 +0300

    first commit

---
+++ src/components/FlipCard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "FlipCard"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PlayField.vue
@@ -0,0 +1,18 @@
+<template>
+  <flip-card />
+</template>
+
+<script>
+import FlipCard from "./FlipCard";
+
+export default {
+  name: "Playground",
+  components: {
+    FlipCard
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
